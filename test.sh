b5b14631f4c1c1a8a1daed3e09ab76986b2a403e
Mauro Song <maurosong@protonmail.com>
2023-08-02 15:49:34 -0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-if [ -z "${BATCH_S3_URL}" ]; then
-    echo "BATCH_S3_URL not set."
-fi
+ls /data/bundle
 
-aws s3 ls "${BATCH_S3_URL}"
+# if [ -z "${BATCH_S3_URL}" ]; then
+#     echo "BATCH_S3_URL not set."
+# fi
+
+# aws s3 ls "${BATCH_S3_URL}"
