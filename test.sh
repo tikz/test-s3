b3d1ba5be2892b7ba85816b02073e3b6178d04af
Mauro Song <maurosong@protonmail.com>
2023-08-04 16:32:25 -0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-aws s3 ls s3://bcovresources/ --recursive --human-readable --summarize
+echo "starting cp"
+aws s3 cp s3://bcovresources/SureSelectHumanAllExonV6/200x/B1316401PTCN.bam /data/bundle/
+echo "cp done"
 
 # if [ -z "${BATCH_S3_URL}" ]; then
 #     echo "BATCH_S3_URL not set."
