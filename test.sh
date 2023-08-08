2632f8d5036b9f6593a9c1aa88a4027504f6aa1e
Mauro Song <maurosong@protonmail.com>
2023-08-08 14:53:45 -0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -7,11 +7,6 @@ lsblk
 echo "done"
 ls /data
 
-echo "cats"
-cat /data/fdisk.txt
-cat /data/lsblk.txt
-cat /data/data/fdisk.txt
-cat /data/data/lsblk.txt
 
 # if [ -z "${BATCH_S3_URL}" ]; then
 #     echo "BATCH_S3_URL not set."
