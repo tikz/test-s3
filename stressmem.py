372ffaff7b00aeb4324127b611a49a9d40f1a4e2
Mauro Song <maurosong@protonmail.com>
2023-09-05 12:09:42 -0300

    Update stressmem.py

--- stressmem.py
+++ stressmem.py
@@ -1,5 +1,10 @@
 import time
 
 time.sleep(5)
-m = bytearray(512000000)
+for i in range(9):
+  print(f"{i} GB test")
+  m = bytearray(i*1024000000)
+  time.sleep(5)
+
+print("reached stress end")
 time.sleep(30)
